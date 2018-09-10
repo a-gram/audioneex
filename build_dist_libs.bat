6f0c1988bef298867955fd311d079830a2a325fd
Albert <alb.gramaglia@audioneex.com>
2018-09-11 00:59:49 +1000

    Update build_dist_libs.bat

--- build_dist_libs.bat
+++ build_dist_libs.bat
@@ -31,7 +31,7 @@ SET BUILD_BTYPE=
 
 for %%a in (win)                   do  if /i "%1"=="%%a" SET BUILD_PLAT=%1
 for %%a in (x32 x64)               do  if /i "%2"=="%%a" SET BUILD_ARCH=%2
-for %%a in (vc10 vc11 vc12 vc141)  do  if /i "%3"=="%%a" SET BUILD_COMP=%3
+for %%a in (vc11 vc12 vc141)       do  if /i "%3"=="%%a" SET BUILD_COMP=%3
 for %%a in (debug release)         do  if /i "%4"=="%%a" SET BUILD_MODE=%4
 for %%a in (static dynamic)        do  if /i "%5"=="%%a" SET BUILD_BTYPE=%5
 
